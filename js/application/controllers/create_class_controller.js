5324be8c0757c620447b3f40a1d77394b22739a1
Partha Protim Konwar <parthaprotimkonwar@gmail.com>
2017-01-15 23:55:18 +0530

    removed unwanted elements of the create class controller

--- js/application/controllers/create_class_controller.js
+++ js/application/controllers/create_class_controller.js
@@ -4,6 +4,33 @@
 myApp.controller('createClassController', ['$scope', '$http', function ($scope, $http) {
 
     $scope.name = "create class controller";
+
+
+    jQuery(document).ready(function($){
+        $('input[name="daterange"]').daterangepicker();
+    });
+
+        /*$(document).ready(function() {
+            $('#date-range-picker').daterangepicker(null, function(start, end, label) {
+                console.log(start.toISOString(), end.toISOString(), label);
+            });
+        });
+        $(document).ready(function() {
+            $('#date-picker').daterangepicker({ singleDatePicker: true }, function(start, end, label) {
+                console.log(start.toISOString(), end.toISOString(), label);
+            });
+        });
+        $(document).ready(function() {
+            $('#date-range-and-time-picker').daterangepicker({
+                timePicker: true,
+                timePickerIncrement: 30,
+                format: 'MM/DD/YYYY h:mm A'
+            }, function(start, end, label) {
+                console.log(start.toISOString(), end.toISOString(), label);
+            });
+        });*/
+
+
 }]);
 
 
